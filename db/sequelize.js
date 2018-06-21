24843ad17df52d247cf42ed9c8ec8e4fdd0b2051
tyt <tangyongtao@gagogroup.com>
2018-06-21 17:08:11 +0800

    feat: orm config

--- db/sequelize.js
+++ db/sequelize.js
@@ -7,6 +7,48 @@ let Sequelize = require('sequelize');
 let env = config.util.getEnv('NODE_ENV') || 'development';
 let logger = require('../util/logger')('SQL');
 
+/**
+ * '$' operation has been deprecated in high version. So you should add this in your code.
+ */
+let Op = Sequelize.Op;
+let operatorsAliases = {
+  $eq: Op.eq,
+  $ne: Op.ne,
+  $gte: Op.gte,
+  $gt: Op.gt,
+  $lte: Op.lte,
+  $lt: Op.lt,
+  $not: Op.not,
+  $in: Op.in,
+  $notIn: Op.notIn,
+  $is: Op.is,
+  $like: Op.like,
+  $notLike: Op.notLike,
+  $iLike: Op.iLike,
+  $notILike: Op.notILike,
+  $regexp: Op.regexp,
+  $notRegexp: Op.notRegexp,
+  $iRegexp: Op.iRegexp,
+  $notIRegexp: Op.notIRegexp,
+  $between: Op.between,
+  $notBetween: Op.notBetween,
+  $overlap: Op.overlap,
+  $contains: Op.contains,
+  $contained: Op.contained,
+  $adjacent: Op.adjacent,
+  $strictLeft: Op.strictLeft,
+  $strictRight: Op.strictRight,
+  $noExtendRight: Op.noExtendRight,
+  $noExtendLeft: Op.noExtendLeft,
+  $and: Op.and,
+  $or: Op.or,
+  $any: Op.any,
+  $all: Op.all,
+  $values: Op.values,
+  $col: Op.col
+};
+
+
 // You need to defined corresponding DB config in config file (development.json/production.json)
 let db = require(`../config/${env}`).db;
 
@@ -23,7 +65,33 @@ let sequelize = new Sequelize(db.database, db.user, db.password, {
     min: 1,
     idle: 10000
   },
-  logging: logging
+  logging: logging,
+  operatorsAliases
 });
+
+// master-slave mode for DB
+
+// let sequelize = new Sequelize(db.database, null, null, {
+//   dialect: 'postgres',
+//   port: '5432',
+//   pool: {
+//     max: 10,
+//     min: 1,
+//     idle: 30000
+//   },
+//   logging: logging,
+//   replication: {
+//     read: [{
+//       username: 'postgres',
+//       password: 'passwd',
+//       host: '172.16.11.13'
+//     }],
+//     write: {
+//       username: 'postgres',
+//       password: 'passwd',
+//       host: '172.16.11.12'
+//     }
+//   }
+// });
 
 module.exports = sequelize;
