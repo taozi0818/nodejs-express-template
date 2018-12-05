5ea56d8c2404ee175a62b76c87b7f9434f7b8aa7
tyt <tangyongtao@gagogroup.com>
2018-12-05 18:59:23 +0800

    feat: validator for request params support RegExp and clean it

--- util/validator.js
+++ util/validator.js
@@ -1,32 +1,28 @@
 /**
  * Created by tyt on 2017/7/24.
- */
-
-let _ = require('lodash');
-
-/**
+ *
  * Request params validator.
  * @param {Object} body - An object need to validate
  * @param {Object} params - Key is a property of `body` need to validate, value is the expected type for `key`
  * @returns {Object} it has a property include all errors' message when occur error, otherwise return null.
  */
 
+let _ = require('lodash');
+
 module.exports = (body, params) => {
   let errArr = [];
-
-  let validate = (value, key, enumFields) => {
-    switch (value.toLowerCase()) {
-    case 'number':
+  let validator = {
+    'number': (key) => {
       body[key] = Number(body[key]);
+
       isNaN(body[key]) ? errArr.push({
         message: `Param \`${key}\` must be Number`,
         type: 'Request Params Invalid',
         param: key,
         value: body[key]
       }) : null;
-      break;
-
-    case 'boolean':
+    },
+    'boolean': (key) => {
       if (typeof body[key] === 'string' && body[key].toLowerCase() === 'true') {
         body[key] = Boolean(true);
       } else if (typeof body[key] === 'string' && body[key].toLowerCase() === 'false') {
@@ -39,37 +35,58 @@ module.exports = (body, params) => {
         param: key,
         value: body[key]
       }) : null;
-      break;
-
-    /**
-     * When you want to validate value whose type is `enum`,
-     * the `params` can be:
-     * { key: ['value'] } or
-     * { key: { type: `enum`, values: [`enumValue`]}}
-     */
-    case 'enum':
-      enumFields.indexOf(body[key]) === -1 ? errArr.push({
-        message: `Value of param \`${key}\` must be in [ ${enumFields} ]`,
+    },
+    'enum': (key, checkValue) => {
+      checkValue.indexOf(body[key]) === -1 ? errArr.push({
+        message: `Value of param \`${key}\` must be in [ ${checkValue} ]`,
         type: 'Request Params Invalid',
         param: key,
         value: body[key]
       }) : null;
-      break;
-
-    case 'required':
+    },
+    'required': (key) => {
       !body[key] ? errArr.push({
         message: `Param \`${key}\` is Required`,
         type: 'Request Params Invalid',
         param: key
       }) : null;
-      break;
+    },
+    'date': (key) => {
+      (new Date(body[key])).toString() === 'Invalid Date' ? errArr.push({
+        message: `Param \`${key}\` is not invalid date`,
+        type: 'Request Params Invalid',
+        param: key,
+        value: body[key]
+      }) : null;
+    },
+    'regexp': (key, checkValue) => {
+      !checkValue.test(body[key]) ? errArr.push({
+        message: `Param \`${key}\` is invalid`,
+        type: 'Request Params Invalid',
+        param: key,
+        value: body[key]
+      }) : null;
+    }
+  };
+
+  let validate = (value, key, checkValue) => {
+    let checkFn = validator[value.toLowerCase()];
+
+    if (checkFn) {
+      checkFn(key, checkValue);
+    } else {
+      /* eslint-disable */
+      console.warn(`The type \`${value}\`of request params \`${key}\` waited to  is in supported list!`);
+      /* eslint-enable */
     }
   };
 
   _.map(params, (paramValue, paramName) => {
     if (typeof paramValue === 'object') {
 
-      if (Array.isArray(paramValue)) {
+      if (paramValue instanceof RegExp) {
+        validate('regExp', paramName, paramValue);
+      } else if (Array.isArray(paramValue)) {
         validate('enum', paramName, paramValue);
       } else {
         validate(paramValue['type'], paramName, paramValue['values']);
